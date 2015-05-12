8eda5077bb773ee176bf9d8aa1981e921512ced9
Reinhard Engels <reinhard.engels@gmail.com>
2015-05-12 10:48:33 -0400

    Initial checkin

---
+++ heroku_django_lti_helloworld/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for heroku_django_lti_helloworld project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "heroku_django_lti_helloworld.settings")
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
